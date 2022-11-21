d7c869c46201f61aea3f44965e3c8a9ca0e9babc
Nikita <malygin.prog@yandex.ru>
2022-11-21 19:03:18 +0500

    *
    *

---
+++ src/components/MyForm.vue
@@ -0,0 +1,185 @@
+<template>
+    <div>
+        <MyHeader :cartItemCount="cartItemCount"></MyHeader>
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-info">
+                    <div class="panel-heading">Pet Depot Checkout</div>
+                    <div class="panel-body">
+                        <div class="form-group">
+                            <div class="col-md-12">
+                                <h4><strong>Enter Your Information</strong></h4>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-6">
+                                <strong>First Name:</strong>
+                                <input
+                                    v-model.trim="order.firstName"
+                                    class="form-control"
+                                />
+                            </div>
+                            <div class="col-md-6">
+                                <strong>Last Name:</strong>
+                                <input
+                                    v-model.trim="order.lastName"
+                                    class="form-control"
+                                />
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-12">
+                                <strong>Address:</strong>
+                            </div>
+                            <div class="col-md-12">
+                                <input
+                                    v-model.trim="order.address"
+                                    class="form-control"
+                                />
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-12"><strong>City:</strong></div>
+                            <div class="col-md-12">
+                                <input
+                                    v-model.trim="order.city"
+                                    class="form-control"
+                                />
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-2">
+                                <strong>State:</strong>
+                                <select
+                                    v-model="order.state"
+                                    class="form-control"
+                                >
+                                    <option disabled value="">State</option>
+                                    <option
+                                        v-for="(state, key) in states"
+                                        v-bind:value="state"
+                                        :key="state"
+                                    >
+                                        {{ key }}
+                                    </option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <strong>Zip / Postal Code:</strong>
+                                <input
+                                    v-model.number="order.zip"
+                                    class="form-control"
+                                    type="number"
+                                />
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-6 boxes">
+                                <input
+                                    type="checkbox"
+                                    id="gift"
+                                    value="true"
+                                    v-bind:true-value="order.sendGift"
+                                    v-bind:false-value="order.dontSendGift"
+                                    v-model="order.gift"
+                                />
+                                <label for="gift">Ship As Gift?</label>
+                            </div>
+                        </div>
+                        <!-- end of form-group -->
+                        <div class="form-group">
+                            <div class="col-md-6 boxes">
+                                <input
+                                    type="radio"
+                                    id="home"
+                                    v-bind:value="order.home"
+                                    v-model="order.method"
+                                />
+                                <label for="home">Home</label>
+                                <input
+                                    type="radio"
+                                    id="business"
+                                    v-bind:value="order.business"
+                                    v-model="order.method"
+                                />
+                                <label for="business">Business</label>
+                            </div>
+                        </div>
+                        <!-- end of form-group-->
+                        <div class="form-group">
+                            <div class="col-md-6">
+                                <button
+                                    type="submit"
+                                    class="btn btn-primary submit"
+                                    v-on:click="submitForm"
+                                >
+                                    Place Order
+                                </button>
+                            </div>
+                            <!-- end of col-md-6-->
+                        </div>
+                        <!-- end of form-group-->
+                        <div class="col-md-12 verify">
+                            <pre>
+                          First Name: {{ order.firstName }}
+                          Last Name: {{ order.lastName }}
+                          Address: {{ order.address }}
+                          City: {{ order.city }}
+                          Zip: {{ order.zip }}
+                          State: {{ order.state }}
+                          Method: {{ order.method }}
+                          Gift: {{ order.gift }}
+                </pre
+                            >
+                        </div>
+                        <!-- end of col-md-12 verify-->
+                    </div>
+                    <!--end of panel-body-->
+                </div>
+                <!--end of panel panel-info-->
+            </div>
+            <!--end of col-md-10 col-md-offset-1-->
+        </div>
+        <!--end of row-->
+    </div>
+</template>
+
+<script>
+import MyHeader from './MyHeader.vue';
+export default {
+    name: 'MyForm',
+    props: ['cartItemCount'],
+    data() {
+        return {
+            order: {
+                firstName: '',
+                lastName: '',
+                address: '',
+                city: '',
+                zip: '',
+                state: '',
+                method: 'Home Address',
+                business: 'Business Address',
+                home: 'Home Address',
+                gift: 'Send As A Gift',
+                sendGift: 'Send As A Gift',
+                dontSendGift: 'Do Not Send As A Gift',
+            },
+            states: {
+                AL: 'Alabama',
+                AR: 'Arizona',
+                CA: 'California',
+                NV: 'Nevada',
+            },
+        };
+    },
+    components: { MyHeader },
+    methods: {
+        submitForm() {
+            alert('Submitted');
+        },
+    },
+};
+</script>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="navbar navbar-default">
+        <div class="navbar-header">
+            <h1>
+                <router-link :to="{ name: 'Main' }">
+                    {{ sitename }}
+                </router-link>
+            </h1>
+        </div>
+        <div class="nav navbar-nav navbar-right cart">
+            <button
+                type="button"
+                class="btn btn-default btn-lg"
+                @click="showCheckout"
+            >
+                <span class="glyphicon glyphicon-shopping-cart">
+                    {{ cartItemCount }}
+                </span>
+                Checkout
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyHeader',
+    data() {
+        return {
+            sitename: 'Vue.JS Pet Depot',
+        };
+    },
+    props: ['cartItemCount'],
+    methods: {
+        showCheckout() {
+            this.$router.push({ name: 'MyForm' });
+        },
+    },
+};
+</script>
+
+<style scoped>
+a {
+    text-decoration: none;
+    color: black;
+}
+</style>
